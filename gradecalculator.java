2d5444868e6bdb348e1180b9d26768b74024336f
rahul <121501082+rah-uls@users.noreply.github.com>
2023-10-28 18:22:22 +0530

    Add files via upload

---
+++ gradecalculator.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class gradecalculator {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n\n: : : WELCOME : : :\n");
+        System.out.println("CODSOFT INTERNSHIP TASK 2\n");
+        System.out.println("CALCULATE THE STUDENT GRADE:\n");
+        System.out.print(":ENTER STUDENT NAME:- ");
+        String name = sc.nextLine();
+        System.out.print("\n:ENTER THE NUMBER OF SUBJECTS:- ");
+        int sub = sc.nextInt();
+        boolean playAgain=true;
+        int total=0;
+        int marks;
+        while(playAgain){
+            for(int i=1;i<=sub;i++){
+                System.out.println();
+                System.out.print(+i+":SUBJECT MARKS- ");
+                marks=sc.nextInt();
+                total=total+marks;
+            }
+            float perc = total/sub;
+            System.out.println("\nYOUR PERCENTAGE OF YOUR SUBJECTS MARKS:- "+perc+"\n");
+            if(perc<=70){
+                System.out.println("YOU PASS IN YOUR EXAM AT FIRST DEVISION 'A' GRADE\n");
+            }
+            else if(perc<=50 || perc>=70){
+                System.out.println("YOU PASS IN YOUR EXAM AT SECOND DEVISION 'B' GRADE\n");
+            }
+            else{
+                System.out.println("YOU PASS IN YOUR EXAM AT THIRD DEVISION 'C' GRADE\n");
+            }
+            System.out.print("DO YOU WANT TO CALCULATE AGAIN THE MARKS-(yes/no)->>");
+            String pA=sc.next();
+            playAgain=pA.equalsIgnoreCase("yes");
+        }
+    }
+}
